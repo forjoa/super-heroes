fac335ba9fc18dc8362d9ab36766092467f86bfb
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2023-06-01 20:01:23 +0200

    some

--- super-heroes/src/App.jsx
+++ super-heroes/src/App.jsx
@@ -4,16 +4,22 @@ function App() {
 
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            HOME
-          </li>
-          <li>
-            STORIES
-          </li>
-        </ul>
-      </nav>
+      <div className='general_content'>
+        <img src='./img/marvel_logo.png' className='logo'></img>
+        <nav>
+          <ul>
+            <li>
+              <a href='index.html'>HOME</a>
+            </li>
+            <li>
+              <a href='stories.html'>STORIES</a>
+            </li>
+            <li>
+              <a href='upload.html'>UPLOAD</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </>
   )
 }
